5c449a3b523b2dcdf5fd89d841b06720d4664c42
Angie <akl5sz@virginia.edu>
2025-06-27 13:36:17 -0400

    create a project schema in sanity

--- next-frontend/sanity.config.ts
+++ next-frontend/sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from "sanity/desk";
+import project from './sanity/schemas/project-schemas';
 
 const config = defineConfig({
     projectId: "7fjdr0vo",
@@ -8,6 +9,7 @@ const config = defineConfig({
     apiVersion: "2025-06-27",
     basePath: "/admin",
     plugins: [deskTool()],
-})
+    schema: { types: [project]  }
+});
 
 export default config;
